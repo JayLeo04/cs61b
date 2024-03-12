dd35bbaadc6117df082e84602ca9453e0a032ca9
JayLeo16 <liutai16@sjtu.edu.cn>
2024-03-12 08:40:20 +0800

    finalversion of lab1 by JayLeo

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,18 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @JayLeo
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if(n == 1){
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        }
+        else if(n % 2 == 1){
+            return 3*n+1;
+        }
+        else{
+            return n/2;
         }
     }
 
